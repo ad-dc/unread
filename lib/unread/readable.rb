aa1900d5560f19233dd76004c0d05ba6429a69be
dan-hoerr <dhoerr@gmail.com>
2013-10-23 14:24:12 -0400

    use unscoped to prevent default_Scope

--- lib/unread/readable.rb
+++ lib/unread/readable.rb
@@ -71,7 +71,7 @@ module Unread
       def reset_read_marks_for_user_with_time(user)
         #find the last updated_at
         if self.readable_options[:new_reader_unread_last]
-          max_time = self.order("updated_at DESC").pluck(:updated_at).first
+          max_time = self.unscoped.order("updated_at DESC").pluck(:updated_at).first
           ReadMark.transaction do
             ReadMark.delete_all :readable_type => self.base_class.name, :user_id => user.id
             ReadMark.create!    :readable_type => self.base_class.name, :user_id => user.id, :timestamp => max_time - 1.second
